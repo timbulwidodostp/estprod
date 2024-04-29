b64702364075c558764e9ab73e8840b794b5b408
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-04-29 21:00:10 +0700

    Use estprod With (In) R Software

---
+++ estprod/estprod.R
@@ -0,0 +1,28 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Levinsohn Petrin Estimation of Production Functions Use estprod With (In) R Software
+install.packages("readxl")
+install.packages("httr")
+install.packages("estprod")
+library("httr")
+library("readxl")
+library("estprod")
+# Import Data Excel Into R From Github Olah Data Semarang (timbulwidodostp)
+github_link <- "https://github.com/timbulwidodostp/estprod/raw/main/estprod/estprod.xlsx"
+temp_file <- tempfile(fileext = ".xlsx")
+req <- GET(github_link, 
+# authenticate using GITHUB_PAT
+authenticate(Sys.getenv("GITHUB_PAT"), ""),
+# write result to disk
+write_disk(path = temp_file))
+estprod <- readxl::read_excel(temp_file)
+# Estimate Levinsohn Petrin Estimation of Production Functions Use estprod With (In) R Software
+levinsohn_model<-levinsohn_petrin(data=estprod,var1~var2|var3|var4,exit=~exit,id="id",time="year",bootstrap=TRUE)
+
+summary(levinsohn_model)
+# Levinsohn Petrin Estimation of Production Functions Use estprod With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
